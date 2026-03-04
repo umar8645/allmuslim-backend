45e91ed6f685b278a5f404e7c5e34d70faa7992c
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-03-03 16:08:30 -0800

    Add RSS status route

--- src/routes/rssRoutes.js
+++ src/routes/rssRoutes.js
@@ -2,6 +2,20 @@ import express from "express";
 import { getRSSFeeds } from "../controllers/rssController.js";
 
 const router = express.Router();
+
+/**
+ * GET /api/rss
+ */
 router.get("/", getRSSFeeds);
+
+/**
+ * GET /api/rss/status
+ */
+router.get("/status", (req, res) => {
+  res.json({
+    rss: "active",
+    message: "RSS system running",
+  });
+});
 
 export default router;
