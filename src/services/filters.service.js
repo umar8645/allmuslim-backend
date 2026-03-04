9ce6a245d30e76803e784deb47ba48d7b74687a4
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-03-04 15:52:20 -0800

    Fix Content model default export (ESM)

--- src/services/filters.service.js
+++ src/services/filters.service.js
@@ -1,8 +1,7 @@
-// src/services/filters.service.js
 export function passFilters({ title = "", description = "" }) {
   const block = /(music|song|mix|prank|shorts|comedy|politics)/i;
   if (block.test(title) || block.test(description)) return false;
 
-  const allow = /(wa'azi|tafseer|qur'an|quran|khutbah|lecture|recitation)/i;
+  const allow = /(wa'azi|waazi|tafseer|qur'an|quran|khutbah|lecture|recitation|karatu|tafsir|islam|hadith|sermon|dars|mahubiri)/i;
   return allow.test(title + " " + description);
 }
--- src/utils/languageFilter.js
+++ src/utils/languageFilter.js
@@ -1,17 +1,17 @@
 const LANGUAGE_HINTS = {
   en: /(islam|quran|lecture|hadith)/i,
-  ar: /[ء-ي]/,
-  ha: /(wa'azi|waazi|musulunci|allah|sunnah|qurani|tafseer|karatu)/i,
+  ar: /[\u0600-\u06FF]/,
+  ha: /(wa'azi|waazi|musulunci|allah|sunnah|qurani|tafseer|karatu|hausa)/i,
   ur: /[؀-ۿ]/,
   fr: /(islam|coran|prêche)/i,
-  tr: /(islam|kuran|vaaz)/i,
+  tr: /(islam|kuran|vaaz|kuranı)/i,
   id: /(islam|kajian|ceramah)/i,
   es: /(islam|corán|hadiz|predica)/i,       // Spanish
   de: /(islam|koran|hadith|vortrag)/i,      // German
   zh: /[\u4e00-\u9fff]/,                    // Chinese
   hi: /(इस्लाम|कुरान|हदीस|वाज़)/,          // Hindi
   ru: /(ислам|коран|хадис|проповедь)/i,     // Russian
-  sw: /(islam|qurani|hadithi|mahubiri)/i    // Swahili
+  sw: /(islam|qurani|hadithi|mahubiri|mhubiri)/i // Swahili
 };
 
 export function isAllowedLanguage(text) {
