764df2fa75794359ce9fb7dcd901710575621628
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-03-09 14:12:46 -0700

    fix searchLectures controller

--- src/controllers/lectureController.js
+++ src/controllers/lectureController.js
@@ -1,12 +1,44 @@
 import Lecture from "../models/Lecture.js"
 import { getTrendingLectures } from "../services/trendingService.js"
 
-export const getLectures = async (req,res)=>{
-  const lectures = await Lecture.find().sort({createdAt:-1})
-  res.json(lectures)
+
+// GET ALL LECTURES
+export const getLectures = async (req, res) => {
+  try {
+    const lectures = await Lecture.find().sort({ createdAt: -1 })
+    res.json(lectures)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-export const getTrending = async (req,res)=>{
-  const lectures = await getTrendingLectures()
-  res.json(lectures)
+
+// GET TRENDING LECTURES
+export const getTrending = async (req, res) => {
+  try {
+    const lectures = await getTrendingLectures()
+    res.json(lectures)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+
+// SEARCH LECTURES
+export const searchLectures = async (req, res) => {
+  try {
+    const { q } = req.query
+
+    if (!q) {
+      return res.json([])
+    }
+
+    const lectures = await Lecture.find({
+      title: { $regex: q, $options: "i" }
+    }).sort({ createdAt: -1 })
+
+    res.json(lectures)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
