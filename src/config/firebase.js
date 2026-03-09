ad47b150d6dbd4bbd9bb0d880ac97eb674d33993
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-03-09 14:22:03 -0700

    add firebase service account

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,23 +1,8 @@
 import admin from "firebase-admin"
-import fs from "fs"
-import path from "path"
+import serviceAccount from "../firebase-service-account.json" assert { type: "json" }
 
-let serviceAccount = null
-
-try {
-  const filePath = path.resolve("src/firebase-service-account.json")
-
-  if (fs.existsSync(filePath)) {
-    serviceAccount = JSON.parse(fs.readFileSync(filePath, "utf8"))
-
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
-    })
-  } else {
-    console.log("⚠ firebase-service-account.json not found, Firebase disabled")
-  }
-} catch (error) {
-  console.log("Firebase init error:", error.message)
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
 
 export default admin
