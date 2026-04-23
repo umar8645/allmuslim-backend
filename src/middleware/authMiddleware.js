0b5f7ebe250394b1540f882ed495d31efb8eb43d
UMAR MUHAMMAD <umarmuhdfata@gmail.com>
2026-04-22 18:55:51 -0700

    fix middleware imports

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "No token provided" });
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
